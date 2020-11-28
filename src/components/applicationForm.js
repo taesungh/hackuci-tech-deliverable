fcebbb5e602b9e703fabef2dbc37548a420a40af
Taesung Hwang <taesunghwang@yahoo.com>
2020-11-27 22:34:27 -0800

    added form validation and made input subcomponents

--- src/components/applicationForm.js
+++ src/components/applicationForm.js
@@ -3,64 +3,124 @@ import React, { useState } from 'react';
 
 const ApplicationForm = () => {
   const [fullName, setFullName] = useState("");
+  const [fullNameValid, setFullNameValid] = useState(null);
+  const checkFullName = (value) => value !== "";
+
   const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(null);
+  // based on RFC 3696 standard
+  const checkEmail = (value) => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/.test(value);
+
   const [funFact, setFunFact] = useState("");
+  const [funFactValid, setFunFactValid] = useState(null);
+  const checkFunFact = (value) => value !== "";
+
+
+  // send form values as GET request to postman-echo server
+  async function sendData() {
+    console.log("sending form data")
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // perform GET request to postman-echo api
+
+    if (fullNameValid && emailValid && funFactValid) {
+      // perform GET request to postman-echo api
+      sendData();
+    }
   };
 
   return (
     <div className="application-form">
       <h2>Hack UCI Application</h2>
-      <form onSubmit={handleSubmit}>
 
-        <div className="form-label-group">
-          <label htmlFor="fullName">Name</label>
-          <input
-            type="text"
-            id="fullName"
-            value={fullName}
-            onChange={(event) => {
-              setFullName(event.target.value);
-            }}
-            placeholder="Name"
-          />
-        </div>
+      <form onSubmit={handleSubmit} noValidate>
 
-        <div className="form-label-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-            placeholder="Email"
-          />
-        </div>
+        <TextInput
+          type="text"
+          label="fullName"
+          labelText="Name"
+          value={fullName}
+          onChange={(event) => {
+            setFullName(event.target.value);
+            setFullNameValid(checkFullName(event.target.value));
+          }}
+          placeholder="Name"
+          invalid={fullNameValid === false}
+          invalidText="Please enter a name."
+        />
 
-        <div className="form-label-group">
-          <label htmlFor="funFact">Fun Fact</label>
-          <textarea
-            id="funFact"
-            value={funFact}
-            onChange={(event) => {
-              setFunFact(event.target.value);
-            }}
-            placeholder="Fun Fact"
-          />
-        </div>
+        <TextInput
+          type="email"
+          label="email"
+          labelText="Email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+            setEmailValid(checkEmail(event.target.value));
+          }}
+          placeholder="Email"
+          invalid={emailValid === false}
+          invalidText="Please enter a valid email address."
+        />
 
-        <button type="submit" className="btn">
+        <TextAreaInput
+          label="funFact"
+          labelText="Fun Fact"
+          value={funFact}
+          onChange={(event) => {
+            setFunFact(event.target.value);
+            setFunFactValid(checkFunFact(event.target.value));
+          }}
+          placeholder="Fun Fact"
+          invalid={funFactValid === false}
+          invalidText="Please enter a fun fact."
+        />
+
+        <button type="submit" className="btn btn-submit">
           Submit
         </button>
 
       </form>
     </div>
   );
+};
+
+const TextInput = ({ type, label, labelText, value, onChange, placeholder, invalid, invalidText }) => {
+  return (
+    <div className="form-label-group">
+      <input
+        type={type}
+        id={label}
+        className={`form-control${invalid ? ' is-invalid' : ''}`}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      <label htmlFor={label}>{labelText}</label>
+      <div className="invalid-feedback">
+        {invalidText}
+      </div>
+    </div>
+  )
+};
+
+const TextAreaInput = ({ label, labelText, value, onChange, placeholder, invalid, invalidText }) => {
+  return (
+    <div className="form-label-group">
+      <textarea
+        id={label}
+        className={`form-control${invalid ? ' is-invalid' : ''}`}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+      <label htmlFor={label}>{labelText}</label>
+      <div className="invalid-feedback">
+        {invalidText}
+      </div>
+    </div>
+  )
 };
 
 export default ApplicationForm;
