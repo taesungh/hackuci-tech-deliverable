316c3c4b5584cbf808b52097352bda390887b6fc
Taesung Hwang <taesunghwang@yahoo.com>
2020-11-27 23:32:18 -0800

    reorganized App components into home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import ApplicationForm from "./components/applicationForm";
-import Mascot from "./components/mascot";
+import Home from "./pages/home";
 
 
 function App() {
   return (
     <div className="App">
-      <ApplicationForm />
-      <Mascot />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ApplicationForm from "../components/applicationForm";
+import Mascot from "../components/mascot";
+
+
+const Home = () => {
+  return (
+    <div className="home">
+      <ApplicationForm />
+      <Mascot />
+    </div>
+  );
+}
+
+export default Home;
